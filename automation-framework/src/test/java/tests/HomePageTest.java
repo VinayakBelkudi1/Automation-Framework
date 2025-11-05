aecd7bc3330359932081eff5da2dba748f08269f
VinayakBelkudi1 <vinayakbelkudi1997@gmail.com>
2025-11-05 11:33:19 +0530

    5th round

--- automation-framework/src/test/java/tests/HomePageTest.java
+++ automation-framework/src/test/java/tests/HomePageTest.java
@@ -13,7 +13,7 @@ public class HomePageTest extends BaseTest {
 		String title=Driver.getTitle();
 		System.out.println("Title of the page is "+title);
 		System.out.println("Feature branch practice");
-		System.out.println("Feature branch practice4444");
+		System.out.println("Feature branch practice555");
 		
 		Assert.assertTrue(title.contains("MakeMyTrip") ,"Remote changes" +title);
 		try {
