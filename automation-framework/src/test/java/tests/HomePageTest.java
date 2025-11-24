97105fc7e728f9c2d4dbab50fd04d14dcc8078f8
VinayakBelkudi1 <vinayakbelkudi1997@gmail.com>
2025-11-24 12:57:35 +0530

    Test cred

--- automation-framework/src/test/java/tests/HomePageTest.java
+++ automation-framework/src/test/java/tests/HomePageTest.java
@@ -13,7 +13,7 @@ public class HomePageTest extends BaseTest {
 		String title=Driver.getTitle();
 		System.out.println("Title of the page is "+title);
 		System.out.println("Feature branch practice");
-		System.out.println("Feature branch practice11/24/2025");
+		System.out.println("Feature branch practice dummy");
 		
 		Assert.assertTrue(title.contains("MakeMyTrip") ,"Remote changes" +title);
 		try {
